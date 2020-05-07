a67fc36425eba70a2e686cb6d284617012398239
tomas <tomas@gamstudio.net>
2020-05-07 16:25:03 +0800

    处理客户端隐藏poker

--- game/internal/algorithm/pk.go
+++ game/internal/algorithm/pk.go
@@ -1,5 +1,7 @@
 package algorithm
 
+import "github.com/name5566/leaf/log"
+
 func (this *Cards) Counter() *ValueCounter {
 	var counter ValueCounter
 	counter.Set(*this)
@@ -16,41 +18,51 @@ func (this *Cards) GetType() uint32 {
 	ASort(*this, 0, int8(len(*this))-1, counter)
 
 	if res := this.royalFlush(); res > 0 {
+		log.Debug("牌值:%v", this.HexInt())
 		return res
 	}
 
 	if res := this.straightFlush(); res > 0 {
+		log.Debug("牌值:%v", this.HexInt())
 		return res
 	}
 
 	if res := this.four(counter); res > 0 {
+		log.Debug("牌值:%v", this.HexInt())
 		return res
 	}
 
 	if res := this.fullFouse(counter); res > 0 {
+		log.Debug("牌值:%v", this.HexInt())
 		return res
 	}
 
 	if res := this.flush(); res > 0 {
+		log.Debug("牌值:%v", this.HexInt())
 		return res
 	}
 
 	if res := this.straight(); res > 0 {
+		log.Debug("牌值:%v", this.HexInt())
 		return res
 	}
 	if res := this.three(counter); res > 0 {
+		log.Debug("牌值:%v", this.HexInt())
 		return res
 	}
 	if res := this.twoPair(); res > 0 {
+		log.Debug("牌值:%v", this.HexInt())
 		return res
 	}
 
 	if res := this.onePair(); res > 0 {
+		log.Debug("牌值:%v", this.HexInt())
 		return res
 	}
 	//高牌（high card）
 	//既不是同一花色也不是同一点数的五张牌组成。
 	//平手牌：如果不止一人抓到此牌，则比较点数最大者，
 	//如果点数最大的相同，则比较第二、第三、第四和第五大的，如果所有牌都相同，则平分彩池。
+	log.Debug("牌值:%v", this.HexInt())
 	return En(HIGH_CARD, ToValue(*this))
 }
