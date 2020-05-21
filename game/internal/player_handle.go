0cf22b86e2e9e456b90b9d67418f7709cd6524d9
tomas <tomas@gamstudio.net>
2020-05-21 20:02:38 +0800

    处理玩家2人开始游戏

--- game/internal/player_handle.go
+++ game/internal/player_handle.go
@@ -11,7 +11,7 @@ func (p *Player) PlayerExitRoom() {
 	v, _ := hall.RoomRecord.Load(rId)
 	if v != nil {
 		room := v.(*Room)
-		if p.totalDownBet > 0 {
+		if p.gameStep == emInGaming {
 			var exist bool
 			for _, v := range room.UserLeave {
 				if v == p.Id {
