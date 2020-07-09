80efc634681173f90486f0c0c84e0968ba27adfa
tomas <tomas@gamstudio.net>
2020-07-09 16:59:23 +0800

    添加机器人问题

--- game/internal/room.go
+++ game/internal/room.go
@@ -688,7 +688,7 @@ func (r *Room) ResultMoney() {
 					sur.TotalLoseMoney += p.LoseResultMoney
 				}
 			}
-			
+
 			// 这里是玩家金额扣税
 			p.resultMoney -= taxMoney
 
@@ -837,7 +837,7 @@ func (r *Room) RobotsLength() int32 {
 func (r *Room) LoadRoomRobots() {
 	// 当玩家创建新房间时,则安排随机2-4机器人
 	rand.Seed(time.Now().UnixNano())
-	num := rand.Intn(1) + 1
+	num := rand.Intn(2) + 2
 	for i := 0; i < num; i++ {
 		time.Sleep(time.Millisecond)
 		robot := gRobotCenter.CreateRobot()
