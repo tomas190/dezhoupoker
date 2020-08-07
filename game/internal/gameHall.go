955318164fc95bbdaa6af13912c21b718c4f6ab8
tomas <tomas@gamstudio.net>
2020-08-07 19:58:11 +0800

    处理玩家坐下玩家显示问题

--- game/internal/gameHall.go
+++ game/internal/gameHall.go
@@ -35,11 +35,11 @@ func HallInit() { // 大厅初始化增加一个房间
 		hall.RoomRecord.Store(r.roomId, r)
 		log.Debug("CreateRoom 创建新的房间:%v", r.roomId)
 
-		r.LoadRoomRobots()
+		//r.LoadRoomRobots()
 
-		//robot := gRobotCenter.CreateRobot()
-		//r.PlayerJoinRoom(robot)
-		//robot.StandUpTable()
+		robot := gRobotCenter.CreateRobot()
+		r.PlayerJoinRoom(robot)
+		robot.StandUpTable()
 	}
 
 }
