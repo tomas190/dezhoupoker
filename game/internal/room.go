3fbb82b646dc0331854be1c7142c3dbe8989981e
tomas <tomas@gamstudio.net>
2020-08-07 19:45:54 +0800

    处理玩家坐下玩家显示问题

--- game/internal/room.go
+++ game/internal/room.go
@@ -777,6 +777,48 @@ func (r *Room) ReadyTimer() {
 			r.counter++
 			log.Debug("readyTime clock : %v ", r.counter)
 			if r.counter == 3 {
+				// 洗牌
+				r.Cards.Shuffle()
+
+				// 设置玩家状态
+				r.SetPlayerStatus()
+
+				// 产生庄家
+				var dealer *Player
+				banker := r.Banker
+				r.Each(int(banker+1)%MaxPlayer, func(p *Player) bool {
+					dealer = p
+					r.Banker = dealer.chair
+					dealer.IsButton = true
+					return false
+				})
+				if dealer == nil {
+					return
+				}
+				log.Debug("庄家的座位号为 :%v", dealer.chair)
+
+				r.remain = 0
+				r.allin = 0
+
+				//Round 1：preFlop 开始发手牌,下注
+				r.readyPlay()
+				r.Status = msg.GameStep_PreFlop
+				log.Debug("GameStep_PreFlop 阶段: %v", r.Status)
+				r.Each(0, func(p *Player) bool {
+					// 生成玩家手牌,获取的是对应牌型生成二进制的数
+					p.cards = algorithm.Cards{r.Cards.Take(), r.Cards.Take()}
+					p.cardData.HandCardKeys = p.cards.HexInt()
+
+					kind, _ := algorithm.De(p.cards.GetType())
+					p.cardData.SuitPattern = msg.CardSuit(kind)
+					log.Debug("preFlop玩家手牌和类型 ~ :%v, %v", p.cards.HexInt(), kind)
+
+					game := &msg.GameStepChange_S2C{}
+					game.RoomData = r.RespRoomData()
+					p.SendMsg(game)
+					return true
+				})
+
 				push := &msg.PushCardTime_S2C{}
 				push.RoomData = r.RespRoomData()
 				r.Broadcast(push)
@@ -825,7 +867,6 @@ func (r *Room) RestartGame() {
 			r.counter++
 			log.Debug("settleTime clock : %v ", r.counter)
 			if r.counter == 4 {
-				r.Status = msg.GameStep_Waiting
 				// 游戏阶段变更
 				game := &msg.GameStepChange_S2C{}
 				game.RoomData = r.RespRoomData()
@@ -834,12 +875,19 @@ func (r *Room) RestartGame() {
 
 			if r.counter >= SettleTime {
 				r.counter = 0
+
 				// 剔除房间玩家
 				r.KickPlayer()
 				// 根据房间机器数量来调整机器
 				r.AdjustRobot()
 				// 超时弃牌站起,这里要设置房间为等待状态,不然不能站起玩家
 				r.TimeOutStandUp()
+
+				r.Status = msg.GameStep_Waiting
+				// 游戏阶段变更
+				game := &msg.GameStepChange_S2C{}
+				game.RoomData = r.RespRoomData()
+				r.Broadcast(game)
 
 				//开始新一轮游戏,重复调用StartGameRun函数
 				log.Debug("RestartGame 开始运行游戏~")
