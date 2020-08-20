782de96ed211cb76f6223a5d6782d1ca0947bd9f
tomas <tomas@gamstudio.net>
2020-08-20 10:21:09 +0800

    处理机器人

--- game/internal/room.go
+++ game/internal/room.go
@@ -908,32 +908,7 @@ func (r *Room) RestartGame() {
 
 				//开始新一轮游戏,重复调用StartGameRun函数
 				log.Debug("RestartGame 开始运行游戏~")
-				//房间内真实玩家数量1名，匹配1~6个机器人
-				//房间内真实玩家数量2名，把2个玩家分开到2个房间，各匹配1~6个机器人
-				//房间内真实玩家数量3名，把3个玩家分开到3个房间，各匹配1~6个机器人
-				//房间内真实玩家数量4名，80%概率直接开始，20%概率回到等待队列重新匹配
-				//房间内真实玩家数量5名，80%概率直接开始，20%概率回到等待队列重新匹配
-				//房间内真实玩家数量6名，80%概率直接开始，20%概率回到等待队列重新匹配
-				//房间内真实玩家数量7名，直接开始
-				//房间内真实玩家数量8名，直接开始
-				//房间内真实玩家数量9名，直接开始
-				//if r.RealPlayerLength() <= 3 {
-				//	for _, v := range r.PlayerList {
-				//		if v != nil {
-				//			hall.PlayerCreateRoom(r.cfgId, v)
-				//		}
-				//	}
-				//	return
-				//}
-				//if r.RealPlayerLength() <= 6 {
-				//	randNum := []int32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-				//	rand.Seed(time.Now().UnixNano())
-				//	num := rand.Intn(len(randNum))
-				//	if randNum[num] >= 9 {
-				//
-				//		return
-				//	}
-				//}
+
 				r.StartGameRun()
 				return
 			}
